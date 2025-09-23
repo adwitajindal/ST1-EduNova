32b66ef3200aaa12dd856877e7f2dc8eeee04a87
adwitajindal <adwita.jindal.42@gmail.com>
2025-09-23 16:25:02 +0530

    .

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -30,7 +30,7 @@ router.post('/AdminReg', adminRegister);
 router.post('/AdminLogin', adminLogIn);
 
 router.get("/Admin/:id", getAdminDetail)
-router.delete("/Admin/:id", deleteAdmin)
+// router.delete("/Admin/:id", deleteAdmin)
 
 // router.put("/Admin/:id", updateAdmin)
 
